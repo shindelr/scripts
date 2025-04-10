e29cd6fc1d9176f6547ecb4aacd0dd9194b4754f
shindelr <129821483+shindelr@users.noreply.github.com>
2025-04-09 22:59:27 -0700

    Added file move functionality to the recently downloaded move script. Still need to add directory functionality

--- recently-downloaded/rdl.py
+++ recently-downloaded/rdl.py
@@ -6,7 +6,7 @@ Move the most recently downloaded file or directory from the downloads to a
 directory of your choosing.
 """
 
-import os, click
+import os, click, shutil
 
 @click.command()
 @click.option('-o', '--out-dir', type=str, default='.')
@@ -15,4 +15,19 @@ def cli(out_dir: str):
     Move the most recently downloaded file or directory from the downloads to a
     directory of your choosing.
     """
-    click.echo("Hey there")
+    file = get_recent_dl()
+    click.echo(f"{file} --> {out_dir}")
+    shutil.move(file, out_dir)
+
+def get_recent_dl() -> str:
+    """Get path to most recent download."""
+    dl_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
+    if os.path.isdir(dl_dir):
+        files = [
+            os.path.join(dl_dir, f) for f in os.listdir(dl_dir)
+            if os.path.isfile(os.path.join(dl_dir, f))
+        ]
+        file = max(files, key=os.path.getctime)
+        return file
+    else:
+        raise NotADirectoryError
--- recently-downloaded/setup.py
+++ recently-downloaded/setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='rdl',
     version='0.1.0',
     py_modules=['rdl'],
-    install_requires=[
-        'Click',
-    ],
+    install_requires=find_packages(),
+    
     entry_points={
         'console_scripts': [
             'rdl = rdl:cli',
